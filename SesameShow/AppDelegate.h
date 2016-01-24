b78abcfaaf63022a8a54bc0707749796b7a54ef4
dqw <dqwdlut@qq.com>
2016-01-24 10:06:41 +0800

    Initial Commit

---
+++ SesameShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SesameShow
+//
+//  Created by dqw on 16/1/24.
+//  Copyright © 2016年 dqw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
